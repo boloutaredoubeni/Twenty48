c2d61bacf66771ee7dbc8d0efc1d8e6c35689b01
Boloutare Doubeni <tarecd@gmail.com>
2016-05-07 21:19:14 -0400

    Add move typing

--- js/components/GameBoard.js
+++ js/components/GameBoard.js
@@ -33,11 +33,24 @@ class Tile extends Component {
 }
 
 export default class GameBoard extends Component {
+
+  x: number;
+  y: number;
+
+  constructor(props: any) {
+    super(props);
+    this.x = 0;
+    this.y = 0;
+  }
+
   render(): React.Element {
+    let tiles = this.props.tiles;
     return (
         // clang-format off
       /* TODO init with tiles */
-      <View>
+      <View
+          onTouchStart={(event: any): void => this._touchStart(event)}
+          onTouchEnd={(event: any): void => this._touchEnd(event)}>
         <View></View>
         <View></View>
         <View></View>
@@ -45,5 +58,31 @@ export default class GameBoard extends Component {
       </View>
       // clang-format on
     );
+  }
+
+  _touchStart(event: any) {
+    this.x = event.nativeEvent.pageX;
+    this.y = event.nativeEvent.pageY;
+    console.log("Start touch event at (" + this.x + "," + this.y + ")");
+  }
+
+  _touchEnd(event: any) {
+    const dx: number = event.nativeEvent.pageX - this.x;
+    const dy: number = event.nativeEvent.pageY - this.y;
+
+    let direction = -1;
+    if (Math.abs(dx) > 3 * Math.abs(dy) && Math.abs(dx) > 30) {
+      direction = dx > 0 ? 2 : 0;
+    } else if (Math.abs(dy) > 3 * Math.abs(dx) && Math.abs(dy) > 30) {
+      direction = dy > 0 ? 3 : 1;
+    }
+
+    if (direction !== -1) {
+      console.log("End touch event at (" + this.x + "," + this.y + ")");
+      // TODO send the swipe event and redraw the view, update the score.
+      console.log("Move in direction " + direction);
+      return;
+    }
+    console.error("Undefined touch event");
   }
 }
